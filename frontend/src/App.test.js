871ceec3959a62c5ce663d0516a520cbf7c8d655
Wael Mashal <wael.mashal@sap.com>
2024-08-21 18:17:45 +0300

    add tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
+// frontend/src/App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeElement = screen.getByText(/Welcome to Our E-commerce Store/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
